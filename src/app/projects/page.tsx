af07f1fabd5ad76d602285bbff8398e759d6cff3
Shubh M Das <shubh.scb@gmail.com>
2025-11-26 11:07:52 +0000

    bug: jsx passing exx fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,12 @@
 import AppCard from "@/components/app-card"
-import { title } from "process";
 import { LuTestTubeDiagonal } from "react-icons/lu";
 
-export function Projects () {
+export function Projects() {
     return (
         <div>
-            <AppCard IconComponent={<LuTestTubeDiagonal />} title="test title" desc="test desc" url="sample url" />
+            <AppCard IconComponent={LuTestTubeDiagonal} title="test title" desc="test desc" url="sample url" />
         </div>
-    )
+    );
 }
 
-export default Projects
+export default Projects;
